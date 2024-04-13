17858e2ff9923a50167307d40bffeab7769c5b0b
Inferno-Team <infernoacm@gmail.com>
2024-04-13 13:06:41 +0300

    update android logo

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,24 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsLogoGen {
+  const $AssetsLogoGen();
+
+  /// File path: assets/logo/logo.cb
+  String get logo => 'assets/logo/logo.cb';
+
+  /// List of all assets
+  List<String> get values => [logo];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsLogoGen logo = $AssetsLogoGen();
+}
